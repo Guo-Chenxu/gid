ebeff080917b1b93325d1e4808c7b0a1efa455a8
郭晨旭 <2269409349@qq.com>
2024-01-06 15:57:08 +0800

    first: init repo

---
+++ src/main/java/com/guochenxu/gid/GidApplication.java
@@ -0,0 +1,13 @@
+package com.guochenxu.gid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guochenxu/gid/GidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guochenxu.gid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
